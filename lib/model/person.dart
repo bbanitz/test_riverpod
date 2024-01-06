dc4dbe17e05fb75cb774447557b6d5010f73c895
bbanitz <bernard@banitz.fr>
2024-01-06 16:20:12 +0100

    refactoring

--- lib/model/person.dart
+++ lib/model/person.dart
@@ -5,45 +5,6 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'person.freezed.dart';
 part 'person.g.dart';
 
-/*
-bool first = true;
-Person person = Person(
-    id: 1,
-    name: 'Bernard',
-    adress: '11a rue des jardins',
-    liste: ['c++', 'java', 'dart'],
-    postalCode: '67800',
-    city: 'Bischheim');
-
-@riverpod
-Person onePerson(OnePersonRef ref) {
-  return person;
-}
-
-@riverpod
-changePerson(ChangePersonRef ref) {
-  print('change person');
-  if (first) {
-    person = Person(
-        id: 1,
-        name: 'Bernard',
-        adress: 'rue du docteur René Barriere',
-        liste: ['c++', 'java', 'dart'],
-        postalCode: '67450',
-        city: 'Drusenheim');
-  } else {
-    person = Person(
-        id: 1,
-        name: 'Bernard',
-        adress: '11a rue des jardins',
-        liste: ['dart', 'flutter', 'riverpod'],
-        postalCode: '67800',
-        city: 'Bischheim');
-  }
-  ref.invalidate(onePersonProvider);
-  first = !first;
-}
-*/
 @freezed
 class Person with _$Person {
   @JsonSerializable(fieldRename: FieldRename.snake)
@@ -54,7 +15,7 @@ class Person with _$Person {
       String? postalCode,
       String? city,
       String? state,
-      required int tics,
+      required int count,
       required List<String> liste}) = _Person;
 
   factory Person.fromJson(Map<String, Object?> json) => _$PersonFromJson(json);
